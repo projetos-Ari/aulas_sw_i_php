bcd3875995dc63c961de15729742e477f7a2420d
Ariane Teruel <80228375+projetos-Ari@users.noreply.github.com>
2022-03-03 08:53:16 -0300

    Add files via upload

---
+++ aula_03_03_22/exemplo_03.php
@@ -0,0 +1,19 @@
+<?php
+    //operadores de comparação
+
+    $a = "10";
+    $b = 10;
+    $soma = $a + $b;
+
+    echo "Usando == :" . ($a == $b);
+    echo "<br>";
+    echo "Usando === :" . ($a === $b);
+    echo "<br>";
+    echo "Somando a + b => " . $soma;
+    echo "<br>";
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+
+
+?>
