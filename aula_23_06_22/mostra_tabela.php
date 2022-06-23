abde7278ca96a4eb2508776ee8ee037a5395af00
Ariane Teruel <80228375+projetos-Ari@users.noreply.github.com>
2022-06-23 08:56:16 -0300

    Add files via upload

---
+++ aula_23_06_22/mostra_tabela.php
@@ -0,0 +1,22 @@
+<?php
+    include_once 'conecta.php';
+
+    $sql = "SELECT * FROM alunos";
+
+    $resultado = mysqli_query($conexao, $sql);
+
+    if(mysqli_num_rows($resultado)>0) {
+    while($linha = mysqli_fetch_assoc($resultado)){
+        echo "<br> id: " . $linha["id"] . "<br>";
+        echo "Nome: " . $linha["nome"] . "<br>";
+        echo "Curso: " . $linha["curso"] . "<br>";
+        echo "Idade: " . $linha["idade"] . "<br>";
+    }
+    }
+    else {
+        echo "0 resultados";
+    }
+
+    mysqli_close($conexao);
+
+?>
